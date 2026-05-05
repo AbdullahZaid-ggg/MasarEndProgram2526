87aefd419516c5d49bc95f4189a2de8321140946
AbdallaZaid <abdallazeed3@gmail.com>
2026-05-05 19:18:44 +0300

    Add logo to navbar and favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import logo from '../assets/logoWithoutTxt.png'
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-logo">
-        <Link to="/">تطوع65x</Link>
+        <Link to="/">
+          <img src={logo} alt="شعار تطوع65x" className="logo-img" />
+          <span className="logo-text">تطوع65x</span>
+        </Link>
       </div>
       <ul className="nav-links">
         <li><Link to="/">الرئيسية</Link></li>
